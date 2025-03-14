29067d6a77efac3374b60a4bb6cf0dbc4567a274
lunaanelia <lunaanelia@gmail.com>
2025-03-14 12:57:06 +0100

    Ahora si lee lo de las 5 paginas solo falta hacerlo funcion

--- Practica1/Ej3/Prueba_de_cosas.py
+++ Practica1/Ej3/Prueba_de_cosas.py
@@ -4,47 +4,56 @@ from bs4 import BeautifulSoup
 def main():
     archivo = open('Ejercicio1.yalm', 'w') 
     
+    num_pag = 3
     url = 'https://quotes.toscrape.com/'
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
+    url_initial  = url
+    for pag in range(num_pag):
+        archivo.write(f"--------------Pagina {pag} --------------\n")
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-    archivo.write("Cita:\n")
+        archivo.write("Cita:\n")
+        cita = soup.find_all(class_='text')    # busca el texto que contiene
+        for i in cita:
+            archivo.write(f"\t- {i.text}\n")
+            #print(f"Citas: {i.text}")
 
-    cita = soup.find_all(class_='text')    # busca el texto que contiene
-    for i in cita:
-        archivo.write(f"\t- {i.text}\n")
-        #print(f"Citas: {i.text}")
+        archivo.write("Autor:\n")
+        autor = soup.find_all(class_ = 'author')
+        for i in autor:
+            archivo.write(f"\t - {i.text}\n")
+        #   print(f"Autor: {i.text}")
 
-    archivo.write("Autor:\n")
-    autor = soup.find_all(class_ = 'author')
-    for i in autor:
-        archivo.write(f"\t - {i.text}\n")
-    #   print(f"Autor: {i.text}")
+        divtags = soup.find_all('div', class_='tags')
+        for i in divtags:
+            enlace = i.find_all('a', class_ = 'tag')
 
-    divtags = soup.find_all('div', class_='tags')
-    for i in divtags:
-        enlace = i.find_all('a', class_ = 'tag')
+            for j in enlace:
+                archivo.write(f"\t- {j.text}\n")
+                # print(f"Etiquetas: {j.text}")
 
-        for j in enlace:
-            archivo.write("Etiquetas:\n")
-            archivo.write(f"\t- {j.text}\n")
-            # print(f"Etiquetas: {j.text}")
+        # Vamoa a buscar el boton y que nos de el siguiente enlace
+        # Todo esto porque es estatico uy no se mueve en la pagina
+        boton_li = soup.find('li', class_='next')
+        urlboton = boton_li.find('a')
+        url = url_initial + urlboton.get('href')
     
     archivo.close()
 
 if __name__ == "__main__":
     main()
 
-# requests.get(url) Obtiene el contenido de la url especificada --> response
-# BeautifulSoup(response.text, 'html.parse') --> Analiza el contenido html con el analizador 
-# soup.prettify() --> Muestra HTML de forma ordenad y legible
+    # requests.get(url) Obtiene el contenido de la url especificada --> response
+    # BeautifulSoup(response.text, 'html.parse') --> Analiza el contenido html con el analizador 
+    # soup.prettify() --> Muestra HTML de forma ordenad y legible
 
-# fiechero yalm debe ser :
-#  Autor:
-#   nombre del autor
-# Cita:
-#   cita de texto:
+    # fiechero yalm debe ser :
+    #  Autor:
+    #   nombre del autor
+    # Cita:
+    #   cita de texto:
 # Tags:
 #   - etiqueta1
 #   - etiqueta2
