7f497015a13e11f0fd3e7ab7e3dfbf33f6674077
lunaanelia <lunaanelia@gmail.com>
2025-03-14 12:41:27 +0100

    Vale tenemos algo pasa pasarlo a clase y que avance 5 paginas pero lo que tenemos va

--- Practica1/Ej3/Prueba_de_cosas.py
+++ Practica1/Ej3/Prueba_de_cosas.py
@@ -2,17 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
+    archivo = open('Ejercicio1.yalm', 'w') 
+    
     url = 'https://quotes.toscrape.com/'
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
 
-    cita = soup.find_all(class_='text')    # busca el texto que contiene
-    #for i in cita:
-    #    print(f"Citas: {i.text}")
+    archivo.write("Cita:\n")
 
+    cita = soup.find_all(class_='text')    # busca el texto que contiene
+    for i in cita:
+        archivo.write(f"\t- {i.text}\n")
+        #print(f"Citas: {i.text}")
+
+    archivo.write("Autor:\n")
     autor = soup.find_all(class_ = 'author')
-    #for i in autor:
+    for i in autor:
+        archivo.write(f"\t - {i.text}\n")
     #   print(f"Autor: {i.text}")
 
     divtags = soup.find_all('div', class_='tags')
@@ -20,7 +27,11 @@ def main():
         enlace = i.find_all('a', class_ = 'tag')
 
         for j in enlace:
-            print(f"Etiquetas: {j.text}")
+            archivo.write("Etiquetas:\n")
+            archivo.write(f"\t- {j.text}\n")
+            # print(f"Etiquetas: {j.text}")
+    
+    archivo.close()
 
 if __name__ == "__main__":
     main()
@@ -38,3 +49,28 @@ if __name__ == "__main__":
 #   - etiqueta1
 #   - etiqueta2
 #   - ...                       Se pone conguiones para indicar lista
+
+# Usando el contexto 'with' para abrir el archivo
+# with open('archivo.txt', 'w') as archivo:
+#     # Escribir varias líneas en el archivo
+#     archivo.write("Este es un ejemplo largo\n")
+#     archivo.write("Primera línea de contenido\n")
+#     archivo.write("Segunda línea de contenido\n")
+#     archivo.write("Tercera línea de contenido\n")
+
+# No es necesario cerrar el archivo explícitamente; Python lo hace automáticamente.
+
+# Abrir archivo en modo escritura
+# archivo = open('archivo.txt', 'w')
+
+# # Escribir contenido en el archivo
+# archivo.write("Este es un ejemplo largo\n")
+# archivo.write("Primera línea de contenido\n")
+# archivo.write("Segunda línea de contenido\n")
+# archivo.write("Tercera línea de contenido\n")
+
+# # Cerrar el archivo manualmente
+# archivo.close()
+
+# En las opciones a es para que añada por donde vaya
+# si borrar lo que tiene el fichero y w sobreescribe
