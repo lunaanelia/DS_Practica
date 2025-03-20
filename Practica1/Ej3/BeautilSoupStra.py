e6ec2e37e10f7cc14875a07223005a6d21bf0132
lunaanelia <lunaanelia@gmail.com>
2025-03-20 13:23:18 +0100

    cosillas

--- Practica1/Ej3/BeautilSoupStra.py
+++ Practica1/Ej3/BeautilSoupStra.py
@@ -17,62 +17,19 @@
 import Strategy
 import requests
 from bs4 import BeautifulSoup
-import yaml
+import yaml             # para escribir en formato yaml
 
-class BeaSoup():
-    def __init__(self, nombre, num):
-        self.nombre = nombre
-        self.num = num
-        self.c_datos = []
-        self.a_datos = []
-        self.e_datos = []
-        self.datos = []
-
-    def poner(self):
-        # odenar primero por autor, luego poner etiqutas y luega tags
-        # como ordenar --> por autor, por cita, por etiquesas, separados,...
-        # self.a_datos.sort()
-        # self.c_datos.sort()
-        # self.e_datos.sort()
+class BeaSoup(Strategy):
+                   
+    def extraer_datos(self, nombre, num):
         
-        citas = {
-            'Citas': self.datos
-        }
-        pepe = yaml.dump(citas, default_flow_style=False,  allow_unicode=True)
-        # pepe = pepe.replace('- ', '    - ') 
-        # pepe = pepe.replace("    - autor", "  - autor")
-        # pepe = pepe.replace("      cita", "    cita")
-        # pepe = pepe.replace("      etiqueta", "    etiquetas")
-        with open(self.nombre, 'w') as archivo:
-            archivo.write(pepe)
-            # for i in range(len(self.datos)):
-        #    pepe = yaml.dump(citas, archivo, default_flow_style=False, indent=4, allow_unicode=True)
-                # archivo.write(f'\t- autor: "{self.a_datos[i]}"\n')
-                # archivo.write(f'\t  cita: "{self.c_datos[i]}"\n')
-                # archivo.write(f"\t  etiquetas:\n")
-                # for j in range(len(self.e_datos[i])):
-                #     archivo.write(f'\t\t- "{self.e_datos[i][j]}"\n')   
-
-            # for autor in self.a_datos:
-            #     archivo.write(f"\t - {autor}\n")
-                
-            
-            # archivo.write("Citas:\n")
-            # for cita in self.c_datos:
-            #     archivo.write(f"\t - {cita}\n")
-            
-            # archivo.write("Error:\n")
-            # for error in self.e_datos:
-            #     archivo.write(f"\t - {error}\n")
-                    
-    def extraer_datos(self):
+        datos = []
         #Realizar la peticion HTTP
         #Realizar extraccion
         url = 'https://quotes.toscrape.com/'
         inicial ='https://quotes.toscrape.com/'
-        # archivo = open(self.nombre, 'w')
 
-        for pag in range(self.num):
+        for pag in range(num):
             response = requests.get(url)
 
             if response.status_code == 200:
@@ -90,30 +47,25 @@ class BeaSoup():
                 for enlace in enlaces:
                     enlacesTexto.append(enlace.text)
 
-                self.datos.append({
+                datos.append({
                     'autor': autor.text,
                     'cita' : cita.text,
                     'etiquetas': enlacesTexto
                 })
-
-                # self.e_datos.append(enlacesTexto)                
-
-            # for cita in citas:
-            #     self.c_datos.append(cita.text)
-            
-
-            # for autor in autores:
-            #     self.a_datos.append(autor.text)
             
             boton = soup.find('li', class_='next')
             urlboton = boton.find('a')
             url = inicial + urlboton.get('href')
 
-        self.poner()
+        citasTexto = {
+            'Citas': datos
+        }
+       
+        with open(nombre, 'w') as archivo:
+            yaml.dump(citasTexto, archivo, default_flow_style=False,  allow_unicode=True)
 
 def main():
-    tre = BeaSoup("archivo.yaml", 1)
-    tre.extraer_datos()
+    BeaSoup().extraer_datos("archivo.yaml", 5)
 
 if __name__ == "__main__":
     main()        
--- Practica1/Ej3/SeleniumStra.py
+++ Practica1/Ej3/SeleniumStra.py
@@ -1,11 +1,60 @@
-# Hemos tenido que descargar
-#       pip install beautifulsoup4
-#       pip instal requests
-
+# pip install selenium
 import Strategy
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+# from selenium.webdriver.firefox.options import Options
 
-class SeleStra(Strategy):
-    def __init__(self, url):
-        #...
-    def extraerInformacion(self):
-        #....
+import yaml 
+
+class Selen():
+    def extraer_datos(self, nombre, num):
+        # options = Options()
+        # options.headless = True  # Activar el modo headless NO abra ventana
+        
+        service = FirefoxService(executable_path = "/snap/bin/geckodriver")
+        driver = webdriver.Firefox(service=service)
+        driver.get("https://quotes.toscrape.com/")
+        
+        # Aquí puedes hacer cualquier interacción con la página
+        
+        page_number = 2
+
+        # Buscar el enlace con el número de la página
+        try:
+            page_link = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.LINK_TEXT, "2"))
+            )
+            page_link.click()  # Hacer clic en el enlace
+            time.sleep(3)  # Esperar a que cargue la página
+        except Exception as e:
+            print(f"No se pudo encontrar el enlace para la página {page_number}")
+
+        
+        citas = driver.find_elements(By.CLASS_NAME, "quote")
+
+
+        # Recorrer todas las citas y mostrar el texto
+        for cita in citas:
+            autor = cita.find_element(By.CLASS_NAME, "author").text
+            texto_cita = cita.find_element(By.CLASS_NAME, "text").text
+            etiquetas = cita.find_elements(By.CLASS_NAME, "tag")
+            
+            etiquetas_texto = [etiqueta.text for etiqueta in etiquetas]
+
+            print(f"Autor: {autor}")
+            print(f"Cita: {texto_cita}")
+            print(f"Etiquetas: {', '.join(etiquetas_texto)}")
+            print("-" * 50)
+        driver.quit()
+
+
+def main():
+    Selen().extraer_datos("archivo.yaml", 5)
+
+if __name__ == "__main__":
+    main()        
