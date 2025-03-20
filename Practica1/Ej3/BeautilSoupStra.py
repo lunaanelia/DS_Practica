ed6d0e20ef05604b6da9d01a2056b21f9dd9e56f
lunaanelia <lunaanelia@gmail.com>
2025-03-20 11:37:39 +0100

    Esto ya esta

--- Practica1/Ej3/BeautilSoupStra.py
+++ Practica1/Ej3/BeautilSoupStra.py
@@ -17,6 +17,7 @@
 import Strategy
 import requests
 from bs4 import BeautifulSoup
+import yaml
 
 class BeaSoup():
     def __init__(self, nombre, num):
@@ -25,27 +26,44 @@ class BeaSoup():
         self.c_datos = []
         self.a_datos = []
         self.e_datos = []
+        self.datos = []
 
     def poner(self):
         # odenar primero por autor, luego poner etiqutas y luega tags
         # como ordenar --> por autor, por cita, por etiquesas, separados,...
-        self.a_datos.sort()
-        self.c_datos.sort()
-        self.e_datos.sort()
-
+        # self.a_datos.sort()
+        # self.c_datos.sort()
+        # self.e_datos.sort()
+        
+        citas = {
+            'Citas': self.datos
+        }
+        pepe = yaml.dump(citas, default_flow_style=False,  allow_unicode=True)
+        # pepe = pepe.replace('- ', '    - ') 
+        # pepe = pepe.replace("    - autor", "  - autor")
+        # pepe = pepe.replace("      cita", "    cita")
+        # pepe = pepe.replace("      etiqueta", "    etiquetas")
         with open(self.nombre, 'w') as archivo:
-            archivo.write("Autores:\n")
-            for autor in self.a_datos:
-                archivo.write(f"\t - {autor}\n")
+            archivo.write(pepe)
+            # for i in range(len(self.datos)):
+        #    pepe = yaml.dump(citas, archivo, default_flow_style=False, indent=4, allow_unicode=True)
+                # archivo.write(f'\t- autor: "{self.a_datos[i]}"\n')
+                # archivo.write(f'\t  cita: "{self.c_datos[i]}"\n')
+                # archivo.write(f"\t  etiquetas:\n")
+                # for j in range(len(self.e_datos[i])):
+                #     archivo.write(f'\t\t- "{self.e_datos[i][j]}"\n')   
+
+            # for autor in self.a_datos:
+            #     archivo.write(f"\t - {autor}\n")
                 
             
-            archivo.write("Citas:\n")
-            for cita in self.c_datos:
-                archivo.write(f"\t - {cita}\n")
+            # archivo.write("Citas:\n")
+            # for cita in self.c_datos:
+            #     archivo.write(f"\t - {cita}\n")
             
-            archivo.write("Error:\n")
-            for error in self.e_datos:
-                archivo.write(f"\t - {error}\n")
+            # archivo.write("Error:\n")
+            # for error in self.e_datos:
+            #     archivo.write(f"\t - {error}\n")
                     
     def extraer_datos(self):
         #Realizar la peticion HTTP
@@ -64,17 +82,28 @@ class BeaSoup():
             autores = soup.find_all(class_='author')
             
             divstags = soup.find_all('div', class_='tags')
-            for divtag in divstags:
-                enlaces = divtag.find_all('a', class_ = 'tag')
-                for enlace in enlaces:
-                    self.e_datos.append(enlace.text)
+           
+            for divtag, cita, autor in zip(divstags, citas, autores) :
+                enlaces = divtag.find_all('a', class_ = 'tag')            
 
-            for cita in citas:
-                self.c_datos.append(cita.text)
+                enlacesTexto = []
+                for enlace in enlaces:
+                    enlacesTexto.append(enlace.text)
+
+                self.datos.append({
+                    'autor': autor.text,
+                    'cita' : cita.text,
+                    'etiquetas': enlacesTexto
+                })
+
+                # self.e_datos.append(enlacesTexto)                
+
+            # for cita in citas:
+            #     self.c_datos.append(cita.text)
             
 
-            for autor in autores:
-                self.a_datos.append(autor.text)
+            # for autor in autores:
+            #     self.a_datos.append(autor.text)
             
             boton = soup.find('li', class_='next')
             urlboton = boton.find('a')
@@ -83,7 +112,7 @@ class BeaSoup():
         self.poner()
 
 def main():
-    tre = BeaSoup("archivo.yaml", 5)
+    tre = BeaSoup("archivo.yaml", 1)
     tre.extraer_datos()
 
 if __name__ == "__main__":
