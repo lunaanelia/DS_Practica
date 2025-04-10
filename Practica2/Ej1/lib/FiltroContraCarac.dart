cd0c0f530416e2d512e40c0646d6ffb3649c5542
nat <natsec@correo.ugr.es>
2025-04-10 18:19:20 +0200

    Cambios locales en configuración Dart SDK

---
+++ Practica2/Ej1/lib/FiltroContraCarac.dart
@@ -0,0 +1,17 @@
+//Tendra que tener un mínimo de 8 caracteres para que de la contraseña como correcta
+class FiltroContraCarac implements Filter{
+
+  @Override
+  public boolean comprueba(Cuenta cuenta) {
+
+    final int TAM_MIN = 8;
+
+    if (cuenta.getContrasena().length() < TAM_MIN) {  // No tiene el tamaño mínimo
+      System.out.printf("\033[0;31m \u0058 Error:\033[0m La contraseña debe contener al menos 8 caracteres\n\n"); //\u0058 es el codigo unicode de X
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+}
