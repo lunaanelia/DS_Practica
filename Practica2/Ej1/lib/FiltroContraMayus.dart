4c4d8290e680c1e2cb4ae4ee22856525672ad050
nat <natsec@correo.ugr.es>
2025-04-10 18:42:18 +0200

    Cambios

---
+++ Practica2/Ej1/lib/FiltroContraMayus.dart
@@ -0,0 +1,21 @@
+import 'Filter.dart';
+
+//Tendra que tener al menos una mayúscula para que de la contraseña como correcta
+class FiltroContraMayus implements Filter{
+
+  @override
+  bool comprueba(Cuenta cuenta){
+
+    String texto=cuenta.getContrasena();
+
+    for(int i=0; i<texto.length; i++){
+      if (texto[i].toUpperCase() == texto[i] && texto[i].toLowerCase() != texto[i]) {
+      return true;
+    }
+
+    //Si ha llegado hasta aqui no contiene mayúsculas
+    return print("Error: La contraseña debe contener al menos un caracter en mayúscula");
+
+  }
+
+}
---
+++ Practica2/Ej1/lib/FiltroContraNum.dart
@@ -0,0 +1,20 @@
+import 'Filter.dart';
+
+//Tendra que tener al menos un caracter numérico para que de la contraseña como correcta
+class FiltroContraNum implements Filter{
+
+  @override
+  bool comprueba(Cuenta cuenta){
+
+    String texto=cuenta.getContrasena();
+
+    for(int i=0; i<texto.length; i++){
+      if (RegExp(r'\d').hasMatch(texto[i]))   //El /d comprueba que es un digito
+        return true;
+    }
+
+    //Si ha llegado hasta aqui no contiene caracteres numéricos
+    print("Error: La contraseña debe contener al menos un caracter numérico");
+  }
+
+}
