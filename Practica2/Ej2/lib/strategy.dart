19c3ff05eacfadd8b512d10d5cb9edc5d8de7883
sofiaglez <sofia.g2co@gmail.com>
2025-04-11 14:35:34 +0200

    cambio comentario

--- Practica2/Ej2/lib/strategy.dart
+++ Practica2/Ej2/lib/strategy.dart
@@ -14,34 +14,6 @@ abstract class Strategy{
       :token = tok,
       url = "https://api-inference.huggingface.co/models/$mod";
 
-/*
-  Future<String> AlgorithmInterface(String text ) async {
-    try{
-        token = await rootBundle.loadString('assets/$token');
-
-        String resultado;
-
-        final url_json = Uri.parse(url); //Convierte un string a Uri
-        final headers = {
-          'Authorization': 'Bearer $token',
-          'Content-Type': 'application/json', //indica que está en formato json
-        };
-
-        final body = jsonEncode({'inputs': text});  //Argumentos que se le pasan al modelo
-
-        final response = await http.post(url_json, headers: headers, body: body);
-
-
-        if (response.statusCode == 200) {   //Respuesta exitosa
-          resultado = jsonDecode(response.body);  //Decodifica la respuesta (de json a String)
-        } else {  //Error
-          resultado = "Error:  ${response.statusCode}\n${response.body}";
-        }
-
-    return resultado;
-  }
-*/
-
   Future<String> AlgorithmInterface(String text) async {
     try {
       // Carga el token desde el archivo
