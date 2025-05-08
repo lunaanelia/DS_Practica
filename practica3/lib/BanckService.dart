82c900963e55edb6646ec243f3abc47bd958e47c
sofiaglez <sofia.g2co@gmail.com>
2025-05-08 12:37:24 +0200

    getUsers, withdraw, deposit

--- practica3/lib/BanckService.dart
+++ practica3/lib/BanckService.dart
@@ -1,9 +1,17 @@
 import 'package:practica3/User.dart';
 import 'package:practica3/Account.dart';
-import 'dart:io'; //-> borrar
+import 'package:practica3/transaction.dart';
+import 'package:practica3/deposit_transaction.dart';
+import 'package:practica3/withdrawal_transaction.dart';
+import 'package:practica3/transfer_transaction.dart';
+
+import 'dart:io';
+
+import 'package:practica3/transaction.dart'; //-> borrar
 
 class BankService {
   final Set<User> _users = {};
+  final Set<Transaction> _transactions ={};
 
   BankService();
 
@@ -30,7 +38,12 @@ class BankService {
   void deposit (String account, double amount) {
     // comprobar si la cuenta exise para algún usuario
 
-    for (var user in this._users ) {
+    Transaction deposit = DepositTransaction(amount);
+    deposit.apply(this._getAccount(account));
+
+
+
+   /* for (var user in this._users ) {
       for (var acc in user.accounts) {
         if (acc.id == account) {
           acc.deposit(amount);
@@ -39,6 +52,35 @@ class BankService {
         }
       }
     }
+    */
+
+
+  }
+
+  void witdraw (String account, double amount) {
+    Transaction withdraw = WithdrawalTransaction(amount);
+    withdraw.apply(this._getAccount(account));
+  }
+
+  void transfer (String from, String to, amount) {
+    Transaction transfer = TransferTransaction(amount,this._getAccount(to));
+    transfer.apply(this._getAccount(from));
+
+  }
+
+
+  Account _getAccount (String account) {
+
+    Account a = Account();
+    for (var user in this._users ) {
+      for (var acc in user.accounts) {
+        if (acc.id == account) {
+          return acc;
+        }
+      }
+    }
+
+    return a; // cambiar esto, esto no se hace así
 
   }
 
@@ -50,6 +92,17 @@ class BankService {
       user.showAccounts();
     }
   }
+
+  List<String> getUsers() {
+    List<String> list = [];
+
+    for (var user in _users) {
+      list.add('${user.id} ${user.name}');
+    }
+
+    return list;
+  }
+
 }
 
 void main () {
@@ -64,6 +117,7 @@ void main () {
 
   bs.showAllUsers();
 
+  print ("DEPOSIT");
   print("Introduzca el ID de una cuenta:");
   String? acc = stdin.readLineSync();
 
@@ -73,6 +127,26 @@ void main () {
 
   if (acc != null && amount != null) {
     bs.deposit(acc, amount);
+  } else {
+    print("Entrada inválida.");
+  }
+
+  bs.showAllUsers();
+
+
+  print ("TRANSFER");
+  print("Introduzca el ID de una cuenta FROM:");
+  acc = stdin.readLineSync();
+
+  print("Introduzca el ID de una cuenta TO:");
+  String? acc2 = stdin.readLineSync();
+
+  print("Introduzca la cantidad a depositar:");
+  amountStr = stdin.readLineSync();
+  amount = double.tryParse(amountStr ?? '');
+
+  if (acc != null && amount != null && acc2 != null) {
+    bs.transfer(acc, acc2, amount);
   } else {
     print("Entrada inválida.");
   }
