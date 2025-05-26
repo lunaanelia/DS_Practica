9036cec0516882575da5e95c4188bacd9805f218
mariarmm <mramosmar03@gmail.com>
2025-05-26 13:26:52 +0200

    ese

--- practica4/libros_pelis/lib/estrategia_autor.dart
+++ practica4/libros_pelis/lib/estrategia_autor.dart
@@ -1,7 +1,8 @@
 import 'package:libros_pelis/gestor.dart';
+import 'package:libros_pelis/i_estrategia_busqueda.dart';
 import 'package:libros_pelis/producto.dart';
 
-class EstrategiaAutor{
+class EstrategiaAutor implements IEstrategiaBusqueda{
 
   @override
   Future<List<Producto>> buscar(Gestor gestor, bool es_peli, String autor){
