7e65fb77eab31a3863b0bf6dc7c0946963662316
mariarmm <mramosmar03@gmail.com>
2025-05-26 12:17:49 +0200

    d

--- practica4/libros_pelis/lib/i_estrategia_busqueda.dart
+++ practica4/libros_pelis/lib/i_estrategia_busqueda.dart
@@ -1,10 +1,5 @@
 
 abstract class IEstrategiaBusqueda{
 
-<<<<<<< HEAD
-
-    List<Producto> buscar(es_peli, titulo, autor, fecha);
-=======
     List<Producto> buscar(bool es_peli, String? titulo, String? autor, String? fecha);
->>>>>>> dd81dd0c3d916c91d22db31a20588d0a56337b43
 }
