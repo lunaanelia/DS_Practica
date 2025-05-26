345c0a704713ad74f1174ac89037a5dba3a46df2
mariarmm <mramosmar03@gmail.com>
2025-05-26 13:47:14 +0200

    correcccion npush

--- practica4/libros_pelis/test/test_maria.dart
+++ practica4/libros_pelis/test/test_maria.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:libros_pelis/estrategia_titulo.dart';
 import 'package:libros_pelis/producto.dart';
 import 'package:libros_pelis/gestor.dart';
 import 'package:libros_pelis/contexto.dart';
@@ -7,9 +8,15 @@ void main () {
 
   group("Test de busqueda", () {
 
-    test("El balance inicial de una cuenta debe ser cero", () {
-      Account cuenta = Account();
-      expect(cuenta.amount, 0);
+    test("Busqueda por titulo", () {
+
+      Gestor g = Gestor();
+      Producto libro = Producto(null,"La casa de Bernarda Alba", "Federico Garcia Lorca", "1936", "La obra refleja la situación social y política en España antes de la Guerra Civil.");
+      g.agregar(libro);
+
+      Contexto contexto = Contexto(g, EstrategiaTitulo());
+
+      expect(contexto.buscar(false, "La casa de Bernarda Alba"), equals(libro));
     });
 
   });
