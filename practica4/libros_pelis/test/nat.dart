21ab4ebd643fb7c9562f29e1cb1ef538a276b579
nat <natsec@correo.ugr.es>
2025-05-27 21:15:28 +0200

    ankdsa

--- practica4/libros_pelis/test/nat.dart
+++ practica4/libros_pelis/test/nat.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:libros_pelis/estrategia_fecha.dart';
 import 'package:libros_pelis/producto.dart';
 import 'package:libros_pelis/estrategia_autor.dart';
-import 'package:libros_pelis/i_estrategia_busqueda.dart';
 import 'package:libros_pelis/gestor.dart';
 import 'package:libros_pelis/contexto.dart';
+import 'package:libros_pelis/factoria.dart';
 
 void main(){
 
@@ -27,6 +28,35 @@ void main(){
 
       expect(res.length, equals(1));
       expect(res[0].autor, equals("Bella"));
+    });
+    test("Busqueda por fecha",()async{
+
+      Gestor gestor= Gestor();
+      Contexto contexto= Contexto(gestor,EstrategiaFecha());
+
+      //Busca si ya existe la peli
+      List<Producto> tmp= await contexto.buscar(true,"12-4-2000");
+
+      if(tmp.isNotEmpty){
+        await gestor.eliminar(tmp[0]);
+      }
+
+
+      Producto peli=Producto(null,true,"Crepusculo","Bella","12-4-2000","Bella, where the hell have you been loca?");
+      await gestor.agregar(peli);
+      List<Producto> res= await contexto.buscar(true,"12-4-2000");
+
+      expect(res.length, equals(1));
+      expect(res[0].autor, equals("Bella"));
+    });
+    test("Busqueda sin coincidencias devuelve vacio",()async{
+
+      Gestor gestor= Gestor();
+      Contexto contexto= Contexto(gestor,EstrategiaAutor());
+
+      List<Producto> res= await contexto.buscar(true,"Test_busqueda");
+
+      expect(res.length, equals(0));
     });
   });
 
@@ -58,6 +88,16 @@ void main(){
 
       await gestor.eliminar(tmp[0]);
 
+    });
+  });
+
+  group("Factoria", (){
+    test("Factoria incorrecta da error",(){
+      Gestor gestor= Gestor();
+
+      Factoria factoria= Factoria(gestor);
+
+      expect( () => factoria.crearProducto("coche", "brum brum", "paco", "12-4-2020", "kachau"),throwsA(isA<ArgumentError>()));
     });
   });
 
