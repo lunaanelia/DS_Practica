06a56b64c77f0a627bc3f8ad80a8600132dc1c0d
nat <natsec@correo.ugr.es>
2025-05-27 18:14:03 +0200

    p

--- practica4/gestor_libros_pelis/app/controllers/producto_controller.rb
+++ practica4/gestor_libros_pelis/app/controllers/producto_controller.rb
@@ -53,7 +53,7 @@ class ProductoController < ApplicationController
 
     private
     def producto_params
-        params.require(:producto).permit(:titulo, :autor, :fecha, :es_peli)
+        params.require(:producto).permit(:titulo, :autor, :fecha, :es_peli, :descripcion)
     end
 
 end
